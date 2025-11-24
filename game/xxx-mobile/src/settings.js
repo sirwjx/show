71609803d3182ba2f17a2fb9a047786897576755
jx <wjx_job1@163.com>
2025-11-24 11:02:14 +0800

    feat: xxx

---
+++ game/xxx-mobile/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/login.fire",orientation:"",jsList:[]};
